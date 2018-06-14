7f5b64490970bac3cc7f49bca5fd774da229db28
mhijack <jianyuan.chen12@gmail.com>
2018-06-14 10:57:55 -0300

    refactor

--- todos_frontend/src/TodoList.js
+++ todos_frontend/src/TodoList.js
@@ -16,146 +16,48 @@ class TodoList extends Component {
     this.loadTodos();
   }
 
-  loadTodos = () => {
-    // GET data
-    fetch(API_URL).then(res => {
-      if (!res.ok) {
-        // If server returns err
-        if (res.status >= 400 && res.status < 500) {
-          return res
-            .json()
-            .then(data => {
-              let err = {
-                errorMessage: data.message
-              };
-              throw err;
-            })
-        } else {
-          // If server err
-          let err = {
-            errorMessage: 'Server not responding. Please try again later.'
-          };
-          throw err;
-        }
-      }
-      // If no err, return data
-      return res.json();
-    }).then(todos => this.setState({todos}))
+  // READ
+  loadTodos = async() => {
+    // Wait for todos be returned from api
+    let todos = await apiCalls.getTodos();
+    this.setState({todos});
   }
 
   // CREATE
-  addTodo = value => {
-    // POST todo to database
-    fetch(API_URL, {
-      method: 'post',
-      headers: new Headers({'Content-Type': 'application/json'}),
-      body: JSON.stringify({name: value})
-    }).then(res => {
-      if (!res.ok) {
-        // If server returns err
-        if (res.status >= 400 && res.status < 500) {
-          return res
-            .json()
-            .then(data => {
-              let err = {
-                errorMessage: data.message
-              };
-              throw err;
-            })
-        } else {
-          // If server err
-          let err = {
-            errorMessage: 'Server not responding. Please try again later.'
-          };
-          throw err;
-        }
-      }
-      // If no err, communicate with database
-      return res.json();
-    }).then(newTodo => this.setState(prevState => {
+  addTodo = async value => {
+    let newTodo = await apiCalls.createTodo(value);
+    this.setState(prevState => {
       return {
         todos: [
           ...prevState.todos,
           newTodo
         ]
       }
-    }))
+    })
   }
 
   // DELETE
-  deleteTodo = id => {
-    const DELETE_URL = `${API_URL}/${id}`;
-    fetch(DELETE_URL, {method: 'delete'}).then(res => {
-      if (!res.ok) {
-        // If server returns err
-        if (res.status >= 400 && res.status < 500) {
-          return res
-            .json()
-            .then(data => {
-              let err = {
-                errorMessage: data.message
-              };
-              throw err;
-            })
-        } else {
-          // If server err
-          let err = {
-            errorMessage: 'Server not responding. Please try again later.'
-          };
-          throw err;
-        }
-      }
-      // If no err, communicate with database
-      return res.json();
-    }).then(() => {
-      const todos = this
-        .state
-        .todos
-        .filter(todo => todo._id !== id);
-      this.setState({todos})
-    })
+  deleteTodo = async id => {
+    await apiCalls.deleteTodo(id);
+    const todos = this
+      .state
+      .todos
+      .filter(todo => todo._id !== id);
+    this.setState({todos});
   }
 
   // PUT
-  toggleTodo = todo => {
-    const UPDATE_URL = `${API_URL}/${todo._id}`;
-    fetch(UPDATE_URL, {
-      method: 'put',
-      headers: new Headers({'Content-Type': 'application/json'}),
-      body: JSON.stringify({
-        completed: !todo.completed
+  toggleTodo = async todo => {
+    const updatedTodo = await apiCalls.toggleTodo(todo);
+    const todos = this
+      .state
+      .todos
+      .map(t => {
+        return t._id === updatedTodo._id
+          ? updatedTodo
+          : t
       })
-    }).then(res => {
-      if (!res.ok) {
-        // If server returns err
-        if (res.status >= 400 && res.status < 500) {
-          return res
-            .json()
-            .then(data => {
-              let err = {
-                errorMessage: data.message
-              };
-              throw err;
-            })
-        } else {
-          // If server err
-          let err = {
-            errorMessage: 'Server not responding. Please try again later.'
-          };
-          throw err;
-        }
-      }
-      // If no err, communicate with database
-      return res.json();
-    }).then(updatedTodo => {
-      const todos = this.state.todos.map(t => {
-        if (t._id === updatedTodo._id) {
-          return updatedTodo;
-        }
-        return t;
-      })
-      this.setState({todos})
-    })
+    this.setState({todos});
   }
 
   render() {
--- todos_frontend/src/api.js
+++ todos_frontend/src/api.js
@@ -1,1 +1,119 @@
 const API_URL = '/api/todos';
+
+export const getTodos = async() => {
+  // GET data
+  return fetch(API_URL).then(res => {
+    if (!res.ok) {
+      // If server returns err
+      if (res.status >= 400 && res.status < 500) {
+        return res
+          .json()
+          .then(data => {
+            let err = {
+              errorMessage: data.message
+            };
+            throw err;
+          })
+      } else {
+        // If server err
+        let err = {
+          errorMessage: 'Server not responding. Please try again later.'
+        };
+        throw err;
+      }
+    }
+    // If no err, return data
+    return res.json();
+  })
+}
+
+export const createTodo = async value => {
+  // POST todo to database
+  return fetch(API_URL, {
+    method: 'post',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify({name: value})
+  }).then(res => {
+    if (!res.ok) {
+      // If server returns err
+      if (res.status >= 400 && res.status < 500) {
+        return res
+          .json()
+          .then(data => {
+            let err = {
+              errorMessage: data.message
+            };
+            throw err;
+          })
+      } else {
+        // If server err
+        let err = {
+          errorMessage: 'Server not responding. Please try again later.'
+        };
+        throw err;
+      }
+    }
+    // If no err, communicate with database
+    return res.json();
+  });
+}
+
+export const deleteTodo = async id => {
+  const DELETE_URL = `${API_URL}/${id}`;
+  return fetch(DELETE_URL, {method: 'delete'}).then(res => {
+    if (!res.ok) {
+      // If server returns err
+      if (res.status >= 400 && res.status < 500) {
+        return res
+          .json()
+          .then(data => {
+            let err = {
+              errorMessage: data.message
+            };
+            throw err;
+          })
+      } else {
+        // If server err
+        let err = {
+          errorMessage: 'Server not responding. Please try again later.'
+        };
+        throw err;
+      }
+    }
+    // If no err, communicate with database
+    return res.json();
+  })
+}
+
+export const toggleTodo = async todo => {
+  const UPDATE_URL = `${API_URL}/${todo._id}`;
+  return fetch(UPDATE_URL, {
+    method: 'put',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify({
+      completed: !todo.completed
+    })
+  }).then(res => {
+    if (!res.ok) {
+      // If server returns err
+      if (res.status >= 400 && res.status < 500) {
+        return res
+          .json()
+          .then(data => {
+            let err = {
+              errorMessage: data.message
+            };
+            throw err;
+          })
+      } else {
+        // If server err
+        let err = {
+          errorMessage: 'Server not responding. Please try again later.'
+        };
+        throw err;
+      }
+    }
+    // If no err, communicate with database
+    return res.json();
+  });
+}
